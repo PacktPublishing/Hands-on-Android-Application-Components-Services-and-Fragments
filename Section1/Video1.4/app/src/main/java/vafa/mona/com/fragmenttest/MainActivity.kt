c4a63300957d093bdb5095c8d65267512fa71fdf
PacktDwayne <dwayned@packtpub.com>
2019-03-26 11:19:53 +0530

    Code files added

---
+++ Section1/Video1.4/app/src/main/java/vafa/mona/com/fragmenttest/MainActivity.kt
@@ -0,0 +1,12 @@
+package vafa.mona.com.fragmenttest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ Section1/Video1.5/app/build/generated/source/buildConfig/debug/vafa/mona/com/fragmenttest/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package vafa.mona.com.fragmenttest;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "vafa.mona.com.fragmenttest";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
