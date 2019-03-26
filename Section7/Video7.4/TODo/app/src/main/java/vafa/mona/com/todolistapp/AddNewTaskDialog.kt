9eeea6097983b13e411afa21dcec6460c7d7ee70
PacktDwayne <dwayned@packtpub.com>
2019-03-26 15:43:47 +0530

    Code files added

---
+++ Section7/Video7.4/TODo/app/src/main/java/vafa/mona/com/todolistapp/AddNewTaskDialog.kt
@@ -0,0 +1,61 @@
+package vafa.mona.com.todolistapp
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.view.View
+import android.widget.EditText
+import kotlinx.android.synthetic.main.dialog_fragment.*
+
+
+/**
+ * Created by ${Mona}
+ */
+
+
+class AddNewTaskDialog : DialogFragment() {
+
+    interface AddNewTaskDialogListener {
+        fun onSubmitClick(dialog: DialogFragment, mDsc: String)
+    }
+    var newTaskDialogListener: AddNewTaskDialogListener? = null
+
+
+    companion object {
+        fun newInstance(): AddNewTaskDialog {
+
+            val newTaskDialogFragment = AddNewTaskDialog()
+            val args = Bundle()
+           // args.putInt("dialog_title")
+            newTaskDialogFragment.arguments = args
+            return newTaskDialogFragment
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(activity)
+        val rootView =
+            activity!!.layoutInflater.inflate(R.layout.dialog_fragment, null)
+        val mTask_dsc = rootView.findViewById<EditText>(R.id.edt_task_dsc)
+
+        builder.setView(rootView)
+            .setPositiveButton("submit") { dialog, id ->
+                newTaskDialogListener?.onSubmitClick(this,mTask_dsc.text.toString() )
+            }
+        return builder.create()
+    }
+
+    override fun onAttach(activity: Activity) {
+        super.onAttach(activity)
+        try {
+            newTaskDialogListener = activity as AddNewTaskDialogListener
+        } catch (e: ClassCastException) {
+            throw ClassCastException(activity.toString() + " must implement AddNewTaskDialogListener")
+        }
+
+    }
+
+
+}
---
+++ Section7/Video7.4/TODo/app/src/main/java/vafa/mona/com/todolistapp/MainActivity.kt
@@ -0,0 +1,55 @@
+package vafa.mona.com.todolistapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.app.DialogFragment
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), AddNewTaskDialog.AddNewTaskDialogListener {
+
+    var taskItems: ArrayList<String> = ArrayList()
+    var mlistView: ListView? = null
+    var mAdapter: ArrayAdapter<String>? = null
+    val mTag : String = "TodoListTask"
+    override fun onSubmitClick(dialog: DialogFragment, mDsc: String) {
+        taskItems.add(mDsc)
+        mAdapter?.notifyDataSetChanged()
+        startService(mDsc)
+    }
+
+    private fun loadListView() {
+        mAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, taskItems)
+        mlistView?.adapter = mAdapter
+    }
+
+    fun startService(mDsc:String) {
+        var intent = Intent(this, MyForegroundService::class.java)
+        intent.putExtra(mTag,mDsc)
+        startService(intent)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        mlistView = findViewById(R.id.main_listview)
+        loadListView()
+        fab.setOnClickListener { view ->
+            addTaskDialog()
+        }
+    }
+
+    fun addTaskDialog() {
+        val mfragmentManager = supportFragmentManager
+        val newFragment = AddNewTaskDialog.newInstance()
+        newFragment.show(mfragmentManager, "newtask")
+    }
+}
